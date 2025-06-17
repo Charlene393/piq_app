6004f48d6b820e846c9e39bd12e9b53da1f60abc
Charlene393 <charlenembugua0@gmail.com>
2025-06-17 23:15:32 +0300

    login screen development

--- user/utils/container/style.tsx
+++ user/utils/container/style.tsx
@@ -1,1 +1,32 @@
-import {View, Text, Image, ScrollView} from 'react-native';
+import { commonStyles } from "@/styles/common.style";
+import { external } from "@/styles/external.style";
+import color from '@/themes/app.colors';
+import { windowHeight, windowWidth } from '@/themes/app.constant';
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        ...commonStyles.shadowContainer,
+        backgroundColor: color.whiteColor,
+        paddingHorizontal: windowHeight(16),
+        paddingVertical: windowHeight(12),
+    },
+    backgroundImage:{
+        width : "100%",
+        height : windowHeight(150),
+        position: "absolute",
+    },
+    contentContainer: {
+        flex: 1,
+        justifyContent: "flex-end",
+    }, 
+    img:{
+        height: windowHeight(28),
+        width: windowHeight(28),
+        ...external.as_center,
+        ...external.mt_50,
+        resizeMode: "contain",
+    },
+
+});
+export default styles;
